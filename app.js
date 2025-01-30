fb3c3ec63dc245f294a3b45df9146a140f18e6c4
jayqss <joellkebret05@gmail.com>
2025-01-30 16:15:54 -0500

    last try

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express(); // ✅ Ensure 'app' is declared before using it
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://hunters-tavern-dl432fv1q-joellkebrets-projects.vercel.app",
+    origin: "https://hunters-tavern-7m6rm28h0-joellkebrets-projects.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
 }));
